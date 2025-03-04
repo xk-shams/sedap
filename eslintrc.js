8feaaf0b255aa5b58121c9a5b51eb663b210be46
xk-shams <olimjonshamsiddinov04@gmail.com>
2025-03-04 16:45:13 +0500

    fixed

---
+++ eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: ["next/core-web-vitals"],
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: "module",
+  },
+  rules: {
+    // eslint rules you want
+  },
+};
